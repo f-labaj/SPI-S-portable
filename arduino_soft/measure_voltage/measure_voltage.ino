38e51f899db83ac9765b7a276384aa1c9769b378
f-labaj <filip.labaj@googlemail.com>
2021-04-20 21:36:33 +0200

    Arduino software

---
+++ arduino_soft/measure_voltage/measure_voltage.ino
@@ -0,0 +1,32 @@
+int analogPin = 1;
+int command = 0;
+int value = 0;
+int meas;
+
+byte b1, b2;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  value = Serial.read();
+  
+  delay(100);
+  
+  if(value == 't') {
+    meas = analogRead(analogPin);
+
+    b1 = meas&0xFF;
+    b2 = ( meas >> 8 ) & 0xFF;
+
+    Serial.write(b1);
+    delay(100);
+    Serial.write(b2);
+    
+    //Serial.println(b1);
+    //Serial.println('\n');
+    //Serial.println(b2);
+    delay(100);
+  }
+}
